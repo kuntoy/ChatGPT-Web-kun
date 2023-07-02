5aad6cd60e75d9a4e91033f4be111811ca740b6d
稻草人 <128972271+kuntoy@users.noreply.github.com>
2023-07-02 15:27:08 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,7 +117,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT Web
         </div>
         <div className={styles["sidebar-sub-title"]}>
           @稻草人
